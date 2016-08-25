8cb1e28307aa74d6f157da4d9aedf29b5c222782
coolsamson7 <andreas.ernst7@gmail.com>
2016-08-25 16:28:43 +0200

    forgiot umbrella headers

---
+++ Inject TVOS/Inject.h
@@ -0,0 +1,19 @@
+//
+//  Inject.h
+//  Inject
+//
+//  Created by Andreas Ernst on 18.07.16.
+//  Copyright © 2016 Andreas Ernst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Inject.
+FOUNDATION_EXPORT double InjectVersionNumber;
+
+//! Project version string for Inject.
+FOUNDATION_EXPORT const unsigned char InjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Inject/PublicHeader.h>
+
+
